112787fdffd1f3290780f23e383c70a8d42ca3ed
ManhPM <phammanhbeo2001@gmail.com>
2023-08-22 21:38:37 +0700

    final

--- models/index.js
+++ models/index.js
@@ -8,13 +8,19 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+require('dotenv').config()
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// == DEFAULT ==
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
+// == DEPOLOY ==
+
+  sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, config);
 
 fs
   .readdirSync(__dirname)
