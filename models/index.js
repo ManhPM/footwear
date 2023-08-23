04bccfe6a6da8a54a0da5314a3eba7a2ada09376
ManhPM <phammanhbeo2001@gmail.com>
2023-08-23 09:44:15 +0700

    test

--- models/index.js
+++ models/index.js
@@ -20,7 +20,10 @@ let sequelize;
 
 // == DEPOLOY ==
 
-  sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, config);
+  sequelize = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialest: "mysql"
+  });
 
 fs
   .readdirSync(__dirname)
