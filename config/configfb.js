b0ea6dde2f256790c77b363b8f3ae4eb0bb9c725
ManhPM <phammanhbeo2001@gmail.com>
2024-03-07 13:23:28 +0700

    new

--- config/configfb.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  FACEBOOK_KEY: "697604285461907", //Type app id here
-  FACEBOOK_SECRET: "9378ddb28cefdbd8da343ae5ca2f59be", //Type app secret here
-  CALLBACK_URL: "http://p2mmilktea.onrender.com/auth/facebook/callback",
-};
--- server.js
+++ server.js
@@ -7,12 +7,6 @@ const XLSX = require("xlsx");
 const port = 4000;
 const app = express();
 const cors = require("cors");
-const configfb = require("./config/configfb");
-const FacebookStrategy = require("passport-facebook").Strategy;
-const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
-const GOOGLE_CLIENT_ID =
-  "975421124869-n4irtjs1qrm9eq8hpddlpo5rma85rihn.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
 require("dotenv").config();
 app.use(express.json());
 const corsOptions = {
@@ -23,56 +17,6 @@ app.use(cors(corsOptions));
 
 app.use(cookieParser());
 app.use("/api/v1", rootRouter);
-
-app.get("/api/v1/svm/recommendation", function (req, res) {
-  // Đọc file
-  const workbook = XLSX.readFile("svm.xlsx");
-  const sheet_name_list = workbook.SheetNames;
-  const worksheet = workbook.Sheets[sheet_name_list[0]];
-
-  // Lấy giá trị từ dòng 1 cột 1 và dòng 1 cột 2
-  const valueA1 = worksheet["A1"].v;
-
-  // Cập nhật giá trị của dòng 1 cột 1
-  worksheet["A1"].v = valueA1 + 1;
-
-  // Ghi lại file
-  XLSX.writeFile(workbook, "svm.xlsx");
-  res.status(200).json({ message: "OK" });
-});
-app.get("/api/v1/svm/recommendation/success", function (req, res) {
-  // Đọc file
-  const workbook = XLSX.readFile("svm.xlsx");
-  const sheet_name_list = workbook.SheetNames;
-  const worksheet = workbook.Sheets[sheet_name_list[0]];
-
-  // Lấy giá trị từ dòng 1 cột 1 và dòng 1 cột 2
-  const valueB1 = worksheet["B1"].v;
-
-  // Cập nhật giá trị của dòng 1 cột 1
-  worksheet["B1"].v = valueB1 + 1;
-
-  // Ghi lại file
-  XLSX.writeFile(workbook, "svm.xlsx");
-  res.status(200).json({ message: "OK" });
-});
-
-app.get("/api/v1/svm", function (req, res) {
-  // Đọc file
-  const workbook = XLSX.readFile("svm.xlsx");
-  const sheet_name_list = workbook.SheetNames;
-  const worksheet = workbook.Sheets[sheet_name_list[0]];
-
-  // Lấy giá trị từ dòng 1 cột 1 và dòng 1 cột 2
-  const valueA1 = worksheet["A1"].v;
-  const valueB1 = worksheet["B1"].v;
-
-  res.status(200).json({
-    totalRecommend: valueA1,
-    totalSuccess: valueB1,
-    percentSuccess: (valueB1 / valueA1) * 100,
-  });
-});
 
 //lắng nghe sự kiện kết nối
 app.listen(port, async () => {
